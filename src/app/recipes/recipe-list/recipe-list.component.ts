2060a31d8a07629180e506dc051326fa4b13b896
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-07-20 16:29:25 +0200

    display mock recipe list in recipe-list component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a test recipe', 'https://images.immediate.co.uk/production/volatile/sites/30/2023/04/Trifle-bowl-coronation-salad-0ad63bf.jpg?quality=90&resize=556,505')
+  ];
 
   constructor() {}
 
